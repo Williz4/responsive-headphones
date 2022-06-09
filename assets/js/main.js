f2db334082fa9070ee5676f377bbc8fa0059e8f8
Wilson Mwangeka <wilsonmwangeka@gmail.com>
2022-06-09 15:17:06 -0700

    ScrollBar Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,7 +44,6 @@ function scrollUp() {
 	const scrollUp = document.getElementById('scroll-up');
 
 	if(this.scrollY >= 200) {
-		console.log('Show');
 		scrollUp.classList.add('show-scroll');
 	} else {
 		scrollUp.classList.remove('show-scroll');
@@ -54,7 +53,24 @@ function scrollUp() {
 window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
